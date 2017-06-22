3efb5ab7ec1aec2a87083a9d7a7b5965a0efdace
shrutisinha <shrutisinhaa952@gmail.com>
2017-06-22 16:25:59 +0530

    corrected code

--- shruti/assignment/data/Data.java
+++ shruti/assignment/data/Data.java
@@ -11,7 +11,14 @@ public class Data {
 	public void printloc(){
 		int x;
 		char y;
-		System.out.println(x);
-		System.out.println(y);
+		/*since x and y are local variables, so they are not 
+		 * initialized by default. We need to initialize them 
+		 * before using. Since its not possible to print the 
+		 * values without initializing as asked by the question,
+		 *  we must either initialize them or comment out the 
+		 *  print statements to prevent error.
+		 */
+		/*System.out.println(x);
+		System.out.println(y);*/
 	}
 }
