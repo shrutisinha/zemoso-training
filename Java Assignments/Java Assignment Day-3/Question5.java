b151b8e07bfb9dadc05647376361e99e089552f5
shrutisinha <shrutisinhaa952@gmail.com>
2017-07-03 18:27:45 +0530

    question 5 corrected again

--- Java Assignments/Java Assignment Day-3/Question5.java
+++ Java Assignments/Java Assignment Day-3/Question5.java
@@ -14,14 +14,15 @@ class Outer{
 		}
 	}
 }
-public class Question5 extends Outer {
- 	class Inner2 extends Inner{
-		Inner2(){
-			super(0);
+
+class Outer2 {
+ 	class Inner2 extends Outer.Inner{
+		Inner2(Outer x){
+			x.super(0);
 			System.out.println("Inner2 ");
 		}
-		Inner2(int n){
-			super(n);
+		Inner2(Outer x, int n){
+			x.super(n);
 			System.out.println("Inner 2");
 		}
 
@@ -29,14 +30,14 @@ public class Question5 extends Outer {
 	
 	// public Inner2 inner() { return new Inner2(); }
 	// public Inner2 inner(int n) { return new Inner2(n); }
-
+} 
+public class Question5{
 	public static void main(String[] args) {
-		Question5 out = new Question5();
+		Outer o1 = new Outer();
 		// Question5.Inner2 x = out.inner();
 		// Question5.Inner2 y = out.inner(5);
 		// Inner2 n = out.inner(5);
-		Inner2 x= out.new Inner2();
-		Inner2 y= out.new Inner2(5);
-	}
-} 
-	
+		Outer2.Inner2 x= new Outer2().new Inner2(o1);
+		Outer2.Inner2 y= new Outer2().new Inner2(o1,5);
+	}	
+}
