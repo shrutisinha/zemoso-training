c142282af2e11b7052ce4230fa62f13d320c05d2
shrutisinha <shrutisinhaa952@gmail.com>
2017-07-07 17:36:00 +0530

    vampire number corrected

--- Java Assignments/Java Assignment Day-2/VampireNumber.java
+++ Java Assignments/Java Assignment Day-2/VampireNumber.java
@@ -9,36 +9,44 @@ import java.util.*;
 import java.lang.*;
 import java.io.*;
 
-public class VampireNumber {
-	static int count=0;
-	static boolean sameChars(String a, String b) {
+class VampireNumberClass {
+	private int count=0;
+	public boolean sameChars(String a, String b) {
 		  char[] first = a.toCharArray();
 		  char[] second = b.toCharArray();
 		  Arrays.sort(first);
 		  Arrays.sort(second);
 		  return Arrays.equals(first, second);
 		}
-	static void generate(ArrayList<Integer> vamp, int n){
+	public void generate(ArrayList<Integer> vamp, int n){
 		int start=(int)Math.pow(10, n/2-1);
 		for(int i=start;i<start*10;i++){
 			for(int j=i;j<start*10;j++){
 				int num= i*j;
-				String a=Integer.toString(num);
-				String b=Integer.toString(i)+Integer.toString(j);
+				String a=String.valueOf(num);
+				String b=String.valueOf(i)+String.valueOf(j);
 				if(sameChars(a,b) ){
 					count++;
 					vamp.add(num);
-					Collections.sort(vamp);
+					// Collections.sort(vamp);
 					//System.out.println(i+" * "+j+" = "+num+"  and count is "+count);
 				}
 			}
 		}
+		Collections.sort(vamp);
 	}
+	public int getCount(){
+		return(count);
+	}
+	
+}
+public class VampireNumber{
 	public static void main(String[] args) {
 		ArrayList<Integer> vamp= new ArrayList<>();
 		int n=2;
-		while (count<100){
-			generate(vamp,n);
+		VampireNumberClass obj=new VampireNumberClass();
+		while (obj.getCount()<100){
+			obj.generate(vamp,n);
 			n+=2;
 		}
 		while(vamp.size()>100)
